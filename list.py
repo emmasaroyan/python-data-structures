ca9c7d3da4afe726e47c3e6bd2e8f6d874f2e48a
Emma Saroyan <emma.saroyan@gmail.com>
2020-02-10 16:34:01 +0400

    update

--- list.py
+++ list.py
@@ -1,19 +1,13 @@
 #The program should build a list of words.
 
-
-
-
 fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
-
 
 for line in fh:
     for word in line.split():
         if word in lst:
             continue;
         lst.append(word)
-
-
 
 print(sorted(lst))

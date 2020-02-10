79f8bdccc92450686860af07e76bb1a662a57d34
Emma Saroyan <emma.saroyan@gmail.com>
2020-02-10 16:28:43 +0400

    assign 4

---
+++ list.py
@@ -0,0 +1,19 @@
+#The program should build a list of words.
+
+
+
+
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+
+
+for line in fh:
+    for word in line.split():
+        if word in lst:
+            continue;
+        lst.append(word)
+
+
+
+print(sorted(lst))
